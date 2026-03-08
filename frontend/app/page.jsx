254d4ef1f8beb756e1091acc6048e9d7a1fc8e3a
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-08 11:29:17 +0530

    Remove text(page.jsx); add heropage.jsx

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import Navabr from "../components/Navbar";
 import Container from "@/components/Container";
+import HeroPage from "@/components/Hero";
 export default function Home() {
   return (
     <Container>
       <section>
       <Navabr />
-      <div className="py-12 text-center w-full h-screen">
-        <h1 className="text-primary text-4xl tracking-tight font-medium">Home Page</h1>
-        
-      </div>
+      <HeroPage />
     </section>  
     </Container>
   );
