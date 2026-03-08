dfe5217453b7b4e321b56621671866b9a6d30da0
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-08 11:28:15 +0530

    Add Herosection

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import Navabr from "../components/Navbar";
+import Container from "@/components/Container";
 export default function Home() {
   return (
-    <section>
+    <Container>
+      <section>
       <Navabr />
-      <div className="py-12px-12 h-screen">
-        <h1 className="text-primary text-4xl tracking-tight font-medium">home Page</h1>
+      <div className="py-12 text-center w-full h-screen">
+        <h1 className="text-primary text-4xl tracking-tight font-medium">Home Page</h1>
         
       </div>
-    </section>
+    </section>  
+    </Container>
   );
 }
---
+++ frontend/components/Hero.jsx
@@ -0,0 +1,16 @@
+const { default: Container } = require("./Container")
+
+const HeroPage = ()=>{
+    return(
+        <div className="">
+            <Container>
+                <h1 className="text-primary">
+                    Hero Page
+                </h1>
+            </Container>
+
+        </div>
+
+    )
+}
+export default HeroPage;
