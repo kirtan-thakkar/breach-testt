c32ea5b5161a9de3b9320a16066ab5036ca75173
jhanvi857 <jhanvip8507@gmail.com>
2026-03-08 12:04:18 +0530

    feat:connect frontend with backend

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
-import Navabr from "../components/Navbar";
+import Navbar from "../components/Navbar";
 import Container from "@/components/Container";
 import HeroPage from "@/components/Hero";
+
 export default function Home() {
   return (
     <Container>
       <section>
-      <Navabr />
-      <HeroPage />
-    </section>  
+        <Navbar />
+        <HeroPage />
+      </section>
     </Container>
   );
 }
--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const { default: Container } = require("./Container");
+import Container from "./Container";
 
 const Navbar = () => {
   return (
