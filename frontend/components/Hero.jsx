96e46d5dbce50e5cac7be4cec0fd0cc563f59848
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-09 22:36:45 +0530

    Add dmSans & instrumental Serif

--- frontend/components/Hero.jsx
+++ frontend/components/Hero.jsx
@@ -1,13 +1,25 @@
-const { default: Container } = require("./Container");
+import { Instrument_Serif } from "next/font/google";
+import Container from "./Container";
+import { DM_Sans } from "next/font/google";
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+const instrumentalSerif = Instrument_Serif({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 const HeroPage = () => {
   return (
     <div className="py-35">
       <Container>
-        <h1 className="text-primary font-medium text-4xl leading-10 tracking-tight text-center">
-          Convert your code into a beautiful image using<br></br>
-          our Ai-Powered Code to <br></br>
-          Image Generator
+        <h1 className={`dmSans ${dmSans.className} text-neutral-700 font-medium text-5xl leading-13 tracking-tight text-center`}>
+          Convert your code into a beautiful image <br></br>
+          using our Ai-Powered Code to <br></br>
+          <span className={`${instrumentalSerif.className} text-7xl`}>
+            Image generator
+          </span>
         </h1>
       </Container>
     </div>
