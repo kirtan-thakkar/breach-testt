f12cd802e07efaf0671aa6057fd75d9ee394cb2d
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-10 19:40:21 +0530

    Add safari mockup

--- frontend/components/Hero.jsx
+++ frontend/components/Hero.jsx
@@ -1,13 +1,19 @@
 "use client";
 import { Instrument_Serif } from "next/font/google";
 import Container from "./Container";
+import { RainbowButton } from "./ui/rainbow-button";
+import { DM_Sans, Geist } from "next/font/google";
+import { Safari } from "./ui/safari";
 import Image from "next/image";
 import { Highlighter } from "@/components/ui/highlighter";
 const instrumentalSerif = Instrument_Serif({
   subsets: ["latin"],
   weight: ["400"],
 });
-
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
 const HeroPage = () => {
   return (
     <div className="py-35">
@@ -16,8 +22,7 @@ const HeroPage = () => {
           <h1
             className={`  font-medium text-6xl  tracking-tighter text-center bg-clip-text text-primary `}
           >
-            Code into a beautiful image <br></br>
-            {" "}
+            Code into a beautiful image <br></br>{" "}
             <Highlighter action="underline" color="#FF9800">
               Ai-Powered Code{" "}
             </Highlighter>
@@ -26,7 +31,7 @@ const HeroPage = () => {
               "Image generator"
             </span>
           </h1>
-          
+
           {/* Can add image either safari mockup according to the problem statement */}
           {/* <Image
             src="/hero.jpg"
@@ -37,13 +42,21 @@ const HeroPage = () => {
           /> */}
 
           <p className="text-secondary text-center tracking-normal w-full max-w-xl">
-            Transform your code into stunning visuals with our AI-powered code to
-            image generator. Whether you're a developer, designer, or content
+            Transform your code into stunning visuals with our AI-powered code
+            to image generator. Whether you're a developer, designer, or content
             creator, our tool allows you to effortlessly convert your code into
           </p>
 
-          
-
+          <RainbowButton
+            size="lg"
+            variant="default"
+            className={`${dmSans.className} text-lg mt-3`}
+          >
+            Get Started
+          </RainbowButton>
+          <div className="w-[1203px]">
+            <Safari url="magicui.design" imageSrc="https://placehold.co/1200x750?text=Hello+World" />
+          </div>
         </div>
       </Container>
     </div>
