57348461b4626492f2c85aa0c4995c693361bd22
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-11 01:27:52 +0530

    Fix inncorrect uri

--- backend/app/langchain/rag.py
+++ backend/app/langchain/rag.py
@@ -5,5 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 os.environ["Groq_API_Key"] = os.getenv("GROQ_API_KEY")
 # print(os.environ["Groq_API_Key"])
-loader = PyPDFLoader("lang.pdf")
+file_path = "C:\\tailwind\\backend\\app\\langchain\\lang.pdf"
+loader = PyPDFLoader(file_path)
 doc = loader.load()
+print(doc[0])
