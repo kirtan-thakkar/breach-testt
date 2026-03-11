97e0e5ff49eee4ff891ffa7b4a21996dd82443b9
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-11 20:21:21 +0530

    Add langchain-groq module & Add recursive text splitter to split the data into chunks from the pdf

--- backend/app/langchain/rag.py
+++ backend/app/langchain/rag.py
@@ -2,10 +2,15 @@ import numpy as np
 import os
 from langchain_community.document_loaders import PyPDFLoader
 from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
 load_dotenv()
 os.environ["Groq_API_Key"] = os.getenv("GROQ_API_KEY")
 # print(os.environ["Groq_API_Key"])
 file_path = "C:\\tailwind\\backend\\app\\langchain\\lang.pdf"
 loader = PyPDFLoader(file_path)
 doc = loader.load()
-print(doc[0])
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunk_data = text_splitter.split_documents(doc)
