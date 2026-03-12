9c8679fb42c8ca385cafc5dc666631168e436161
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-12 17:39:50 +0530

    Add motion -> heroPage

--- frontend/components/Hero.jsx
+++ frontend/components/Hero.jsx
@@ -2,11 +2,12 @@
 import { Instrument_Serif } from "next/font/google";
 import Container from "./Container";
 import { RainbowButton } from "./ui/rainbow-button";
-import { DM_Sans, Geist } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import { Safari } from "./ui/safari";
 import Image from "next/image";
 import { Highlighter } from "@/components/ui/highlighter";
 import ScrollReveal from "./TextReveal";
+import { motion } from "motion/react";
 import ChoosePage from "./Choose";
 const instrumentalSerif = Instrument_Serif({
   subsets: ["latin"],
@@ -18,42 +19,101 @@ const dmSans = DM_Sans({
 });
 const HeroPage = () => {
   return (
-    <div className="py-35">
+    <div className="py-24">
       <Container>
         <div className="flex flex-col items-center justify-center gap-6">
-          <h1
-            className={`  font-medium text-6xl  tracking-tighter text-center text-primary `}
+          <motion.div
+            initial={{
+              opacity: 0,
+              filter: "blur(12px)",
+            }}
+            whileInView={{
+              opacity: 1,
+              filter: "blur(0px)",
+            }}
+            transition={{
+              duration: 0.4,
+              ease: "easeOut",
+            }}
           >
-            Code into a beautiful image <br></br>{" "}
-            <Highlighter action="underline" color="#FF9800">
-              Ai-Powered Code{" "}
-            </Highlighter>
-            &nbsp;to<br></br>
-            <span className={`${instrumentalSerif.className} text-7xl`}>
-              "Image generator"
-            </span>
-          </h1>
-
-          <p className="text-lg text-secondary text-center tracking-normal w-full max-w-xl">
+            <h1
+              className={`  font-medium text-6xl  tracking-tighter text-center text-primary `}
+            >
+              Code into a beautiful image <br></br>{" "}
+              <Highlighter action="underline" color="#FF9800">
+                Ai-Powered Code{" "}
+              </Highlighter>
+              &nbsp;to<br></br>
+              <span className={`${instrumentalSerif.className} text-7xl`}>
+                "Image generator"
+              </span>
+            </h1>
+          </motion.div>
+          <motion.p
+            initial={{
+              opacity: 0,
+              filter: "blur(12px)",
+            }}
+            whileInView={{
+              opacity: 1,
+              filter: "blur(0px)",
+            }}
+            transition={{
+              duration: 0.3,
+              delay: 0.2,
+            }}
+            className="text-lg text-secondary text-center tracking-normal w-full max-w-xl"
+          >
             Transform your code into stunning visuals with our AI-powered code
             to image generator. Whether you're a developer, designer, or content
             creator, our tool allows you to effortlessly convert your code into
-          </p>
+          </motion.p>
 
-          <RainbowButton
-            size="lg"
-            variant="default"
-            className={`${dmSans.className} text-lg mt-3`}
+          <motion.div
+            initial={{
+              opacity: 0,
+              filter: "blur(12px)",
+            }}
+            whileInView={{
+              opacity: 1,
+              filter: "blur(0px)",
+            }}
+            transition={{
+              duration: 0.3,
+              delay: 0.4,
+            }}
           >
-            Get Started
-          </RainbowButton>
+            <RainbowButton
+              size="lg"
+              variant="default"
+              className={`${dmSans.className} text-lg mt-3`}
+            >
+              Get Started
+            </RainbowButton>
+          </motion.div>
 
-          <div className="py-14 w-300.75">
+          <motion.div
+          initial={{
+            y:30,
+            opacity:0,
+            filter:"blur(10px)"
+          }} 
+          whileInView={{
+            opacity:1,
+            y:0,
+            filter:"blur(0px)",
+          }}
+          transition={{
+            duration:0.3,
+            delay:0.5,
+            ease:"easeInOut"
+          }}
+          className="py-14 w-300.75">
             <Safari
               url="magicui.design"
               imageSrc="https://placehold.co/1200x750?text=Hello+World"
             />
-          </div>
+          </motion.div>
 
           {/* Add textReveal component by urself if that does not work iterate with below */}
           {/* <div className="h-screen flex justify-center items-center">
@@ -70,10 +130,26 @@ const HeroPage = () => {
               the digital world with confidence and agility.
             </ScrollReveal>
           </div> */}
-          <p className="py-18 mt-6  text-primary text-center tracking-tighter leading-13 max-w-4xl text-5xl font-medium ">
-            Simplicity, performance, and security,<br></br> empowering you to navigate
-            the digital <br></br>world with confidence and agility.
-          </p>
+          <motion.h2
+          initial={{
+            opacity:0,
+            filter:"blur(12px)",
+            y:15
+          }}
+          whileInView={{
+            opacity:1,
+            filter:"blur(0px)",
+            y:0
+          }}
+          transition={{
+            duration:0.3,
+            delay:0.3,
+            ease:"easeInOut"
+          }}
+          className="py-18 mt-6  text-primary text-center tracking-tighter leading-13 max-w-4xl text-5xl font-medium ">
+            Simplicity, performance, and security,<br></br> empowering you to
+            navigate the digital <br></br>world with confidence and agility.
+          </motion.h2>
         </div>
         <div>
           <ChoosePage />
